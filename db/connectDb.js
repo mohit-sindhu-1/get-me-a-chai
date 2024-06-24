e12dc5136b03cadb0af00ac8cf01a59a5cbb2085
mohit <mohitsindhu2003@gmail.com>
2024-06-24 20:25:32 +0530

    update mongo atlas uri

--- db/connectDb.js
+++ db/connectDb.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect('mongodb://localhost:27017/Chai');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
