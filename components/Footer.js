cb6a51c66d6ebdbe5b42c58ed6dfd1233d3790d2
mohit <mohitsindhu2003@gmail.com>
2024-11-10 08:04:37 +0530

    Reordering folder structure

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <footer className='bg-slate-900 text-white px-4 h-12 flex items-center justify-center'>
-      <p className='md:text-base text-xs'>Copyright &copy; {currentYear} Get me a Chai - All rights reserved!</p>
+      <p className='md:text-base text-xs'>Copyright &copy; {currentYear} Get me a Chai - All rights reserved</p>
     </footer>
   )
 }
